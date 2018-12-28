390ab3de37409cd73cadfbdb4662a9a21b1a6e5f
bkawk <email.will.in.china@gmail.com>
2018-12-28 20:23:41 +0800

    make fair

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ async function getTodoAsync() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
     const json = await response.json();
-    console.log(`b: ${timesTen(json.id)}`);
+    const sum = await timesTen(json.id);
+    console.log(`b: ${sum}`);
   } catch (err) {
     console.log(err.message);
   }

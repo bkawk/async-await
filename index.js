aba51e9e91c0f79f7759ea64809a7fc48c6f90b6
bkawk <email.will.in.china@gmail.com>
2018-12-28 15:07:23 +0800

    example one

---
+++ index.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+
+/**
+ * getTodo Promise
+ */
+function getTodoPromise() {
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+}
+
+/**
+ * getTodo Async
+ */
+async function getTodoAsync() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const json = await response.json();
+    console.log(json);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+getTodoPromise();
+getTodoAsync();
